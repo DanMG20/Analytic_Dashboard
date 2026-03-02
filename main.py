6f0284da81c1bc293f3503b951da563b8fb508c5
Edgar Molina <EDMG0220@outlook.com>
2026-03-02 17:30:14 -0600

    -Arquitechture Lay Out defined -YT API OUAUTH2.0 implemented

--- main.py
+++ main.py
@@ -1,35 +1,10 @@
-from youtube_conector import YoutubeConnector
-from youtube_requester import YoutubeRequester
-
-#Direcciones 
-CLIENT_SECRET="credenciales/credenciales_google.json"
-TOKEN_FILE="credenciales/token.json"
-SCOPES = ["https://www.googleapis.com/auth/youtube.readonly"]
-
+from api.youtube_client import YouTubeClient
 
 def main():
+    yt = YouTubeClient()
+    channel_info = yt.get_my_channel()
 
-    #Paso 1 Conexion
+    print(channel_info)
 
-    #Pasamos los datos del cliente mediante un objeto conexion 
-    conexion = YoutubeConnector(
-        client_secret=CLIENT_SECRET,
-        token_file=TOKEN_FILE,
-        scopes=SCOPES,
-        env ="development"
-    )
-    #autentica los datos 
-    conexion.authenticate()
-
-    #Paso 2 Requisición de datos 
-    #creamos el objeto que nos va a solicitar los datos de cualquiera de las dos apis
-    requisitor = YoutubeRequester(youtube_analytics=conexion.youtube_analytics,youtube_data=conexion.youtube_data)
-
-    print(requisitor.obtener_parametros_principales())
-
-
-
-
-
-if __name__ == "__main__": 
+if __name__ == "__main__":
     main()
